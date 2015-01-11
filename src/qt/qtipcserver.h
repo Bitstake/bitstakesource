3341947e435977679020be65eb702e19ed794f40
Bitstake <bitstake@gmail.com>
2015-01-10 21:59:52 -0500

    bitstake
    source

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Bitstake-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BitstakeURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
